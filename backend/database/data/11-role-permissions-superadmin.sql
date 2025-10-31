c53ba23ffe17c9f5a25a6b57295e5d861106d7f5
Benno2406 <31268711+Benno2406@users.noreply.github.com>
2025-10-31 18:21:51 +0100

    v0

--- backend/database/data/11-role-permissions-superadmin.sql
+++
@@ -1,8 +0,0 @@
--- FMSV Dingden - Role Permissions: Superadmin
--- Super-Administrator: ALLE Berechtigungen
-
-INSERT INTO role_permissions (role_id, permission_id)
-SELECT r.id, p.id
-FROM roles r
-CROSS JOIN permissions p
-WHERE r.name = 'superadmin';
--- backend/database/data/12-role-permissions-admin.sql
+++
@@ -1,9 +0,0 @@
--- FMSV Dingden - Role Permissions: Admin
--- Administrator: Fast alle (außer system.database_access)
-
-INSERT INTO role_permissions (role_id, permission_id)
-SELECT r.id, p.id
-FROM roles r
-CROSS JOIN permissions p
-WHERE r.name = 'admin'
-AND p.name != 'system.database_access';
